7d29ca2f3593f8410c0f2f50d60e519c52c97973
Larissa Pinheiro <85531036+iaarkz@users.noreply.github.com>
2023-01-31 14:35:41 -0300

    Add files via upload

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,19 @@
+import './ExpanseDate.css';
+
+function ExpenseDate(props) {
+	const day = props.date.toLocaleString('pt-BR', { day: '2-digit' });
+	const month = props.date.toLocaleString('pt-BR', { month: 'long' });
+	const year = props.date.getFullYear();
+
+	return (
+		<div className='expense-date'>
+			<div className='expense-date__day'>{day}</div>
+			<div className='expense-date__month'>
+				{month[0].toUpperCase() + month.slice(1)}
+			</div>
+			<div className='expense-date__year'>{year}</div>
+		</div>
+	);
+}
+
+export default ExpenseDate;
